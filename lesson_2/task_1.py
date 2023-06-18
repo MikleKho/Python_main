935c46a5f7ba490b72ed0910ca3595f0bff76554
MikleKho <109470069+MikleKho@users.noreply.github.com>
2023-06-18 14:00:19 +0300

    Fixed after review

--- lesson_2/task_1.py
+++ lesson_2/task_1.py
@@ -1,8 +1,7 @@
 # Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление. Функцию hex используйте для проверки своего результата.
 
 
-def rebase_number(number_in, base):
-    BASE_SYMBOLS = ['0', '1', '2', '3', '4','5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+def rebase_number(number_in, base, BASE_SYMBOLS):
     number_str_out = []
     while number_in != 0:
         number_str_out.append(BASE_SYMBOLS[number_in % base])
@@ -13,7 +12,8 @@ def rebase_number(number_in, base):
 
 if __name__ == '__main__':
     base = 16
+    BASE_SYMBOLS = '0123456789ABCDEF'
     number_in = int(input('Ведите целое десятичное число для преобразования: '))
-    print(f'Результат преобразования: {rebase_number(number_in, base)}')
+    print(f'Результат преобразования: {rebase_number(number_in, base, BASE_SYMBOLS)}')
     print(f'Проверка: {hex(number_in)}')
 
